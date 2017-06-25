24b04214879d496474bbc216d5c9fadfa9688bda
marcamesser <marcamesser@gmail.com>
2017-06-25 16:21:10 -0400

    Added class and program file
    Very simple timestamp class to take messages and add system
    time/username

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleTimestamp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            TimeStamp timeStamp;
+            timeStamp = new TimeStamp();
+            timeStamp.TsMessage("mmesser","Hello World");
+        }
+    }
+}
